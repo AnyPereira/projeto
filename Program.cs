9224e0f9d6665c5b1edc4f8945ea94e3916a425e
Any <55221912+AnyPereira@users.noreply.github.com>
2019-10-13 22:42:21 -0300

    Projeto conexão com o banco

--- Program.cs
+++ Program.cs
@@ -13,40 +13,32 @@ namespace ConsoleApp2
 
         static void Main()
         {
-            // 1. Instancia a conexão(objeto SqlConnection)
             SqlConnection conn = new SqlConnection("workstation id=anysantos.mssql.somee.com;packet size=4096;user id=anypsantos;pwd=@Legal2019;data source=anysantos.mssql.somee.com;persist security info=False;initial catalog=anysantos");
-            //
-            // define um SqlDataReader nulo
+                       
             SqlDataReader dr = null;
             try
-            {
-                // 2. Abre a conexão
+            {               
                 conn.Open();
-                // 3. Passa conexão para o objeto command
                 SqlCommand cmd = new SqlCommand("select * from cliente", conn);
-                //
-                // 4. Usa conexão
-                // obtêm o resultado da consulta
                 dr = cmd.ExecuteReader();
-                // imprime o codigo do cliente para cada registro
+                
                 while (dr.Read())
                 {
-                    Console.WriteLine(dr[0]);
-                    Console.WriteLine(dr[1]);
-                    Console.WriteLine(dr[2]);
-                    Console.WriteLine(dr[3]);
-                    Console.WriteLine(dr[4]);
+                    Console.Write(dr[0]);
+                    Console.Write(dr[1]);
+                    Console.Write(dr[2]);
+                    Console.Write(dr[3]);
+                    Console.Write(dr[4]);
                 }
                 Console.ReadKey();
             }
             finally
             {
-                // fecha o reader
                 if (dr != null)
                 {
                     dr.Close();
                 }
-                // 5. Fecha a conexão
+                
                 if (conn != null)
                 {
                     conn.Close();
