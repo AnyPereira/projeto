a2e6915136398e173f530890561314a39e2e744d
Any <55221912+AnyPereira@users.noreply.github.com>
2019-10-18 13:20:44 -0300

    usando conexão com dapper

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using Dapper;
+using System.Collections;
 
 namespace ConsoleApp2
 {
@@ -15,26 +16,47 @@ namespace ConsoleApp2
 
         static string conn = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
 
+        public class Cliente
+        {
+            public int clienteId { get; set; }
+            public string nome { get; set; }
+            public string sobrenome { get; set; }
+            public DateTime data_Nasc { get; set; }
+            public int RG { get; set; }
+            public Endereco Endereco { get; set; }
+        }
+
+        public class Endereco
+        {
+            public int enderecoId { get; set; }
+            public int clienteId { get; set; }
+            public string logradouro { get; set; }
+            public int numero { get; set; }
+            public string complemento { get; set; }
+            public string ponto_De_Referencia { get; set; }
+            public IEnumerable <Cliente> clientes { get; set; }
+        }
         private static void Main()
         {
-
-
-            SqlConnection conexaoBD = new SqlConnection(conn);
-            conexaoBD.Open();
-            var resultado = conexaoBD.Query("Select * from cliente");
-            Console.WriteLine("{0} - {1} - {2} - {3} - {4} ", "Código", "Nome", "Sobrenome", "Data_Nasc", "RG");
-            foreach (dynamic cliente in resultado)
+            using (var conexao = new SqlConnection(conn))
             {
-                Console.WriteLine("{0} - {1} - {2} - {3} - {4} ", cliente.clienteID, cliente.Nome, cliente.Sobrenome, cliente.Data_Nasc, cliente.RG);
+                IEnumerable clientes = conexao.Query<Cliente>("select * from cliente");
+
+                Console.WriteLine("{0} - {1} - {2} - {3} - {4}", "Código", "Nome", "Sobrenome", "Data de Nacimento", "RG");
+                foreach(Cliente cliente in clientes)
+                {
+                    Console.WriteLine("{0} - {1} - {2} - {3} - {4} ", cliente.clienteId, cliente.nome, cliente.sobrenome, cliente.data_Nasc, cliente.RG);
+                }
+
+                IEnumerable enderecos = conexao.Query<Endereco>("select * from endereco");
+
+                Console.WriteLine("{0} - {1} - {2} - {3} - {4} - {5}", "Código Endereco", "Código Cliente", "Endereço", "Número", "Complemento", "Ponto de Referência");
+                foreach (Endereco endereco in enderecos)
+                {
+                    Console.WriteLine("{0} - {1} - {2} - {3} - {4} - {5} ", endereco.enderecoId, endereco.clienteId, endereco.logradouro, endereco.numero, endereco.complemento, endereco.ponto_De_Referencia);
+                }
             }
-            var resultadoEnd = conexaoBD.Query("Select * from endereco");
-            Console.WriteLine("{0} - {1} - {2} - {3} - {4} - {5}", "Código", "Código Cliente", "Logradouro", "Número", "Complemento", "Ponto de Referência");
-            foreach (dynamic endereco in resultadoEnd)
-            {
-                Console.WriteLine("{0} - {1} - {2} - {3} - {4} - {5}", endereco.enderecoId, endereco.clienteId, endereco.Logradouro, endereco.Numero, endereco.Complemento, endereco.Ponto_de_referencia);
-            }
-            Console.ReadKey();
-            conexaoBD.Close();
+                Console.ReadKey();            
         }
     }
 }
